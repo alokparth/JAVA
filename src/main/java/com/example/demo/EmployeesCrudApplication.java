936b10866e47f8f56032aab988d38149eab8a9b0
Parth Alok <alokparth703@gmail.com>
2023-10-02 01:04:34 +0530

    final commit

--- src/main/java/com/example/demo/EmployeesCrudApplication.java
+++ src/main/java/com/example/demo/EmployeesCrudApplication.java
@@ -8,10 +8,10 @@ import org.springframework.core.SpringVersion;
 public class EmployeesCrudApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(EmployeesCrudApplication.class, args);
-		SpringApplication springApplication=new SpringApplication(EmployeesCrudApplication.class);
-	      System.out.println("Spring Core Version:- " + SpringVersion.getVersion());
-	      springApplication.run(args);
+		SpringApplication.run(EmployeesCrudApplication.class, args);
+//		SpringApplication springApplication=new SpringApplication(EmployeesCrudApplication.class);
+//	      System.out.println("Spring Core Version:- " + SpringVersion.getVersion());
+//	      springApplication.run(args);
 	}
 
 }
--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,13 +44,32 @@ public class EmployeeController {
 		else return new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
 	}
 	@PutMapping("/employees/{empid}")
-	public String updateVal(@PathVariable Long empid) {
-		Optional<Employee> emp=employeerepository.findById(empid);
-		if(emp.isPresent()) {
-			Employee enw=emp.get();
+	public String updateVal(@PathVariable Long empid,@RequestBody Employee employee) {
+		Optional<Employee> emw=employeerepository.findById(empid);
+		if(emw.isPresent()) {
+			Employee emp=emw.get();
+			emp.setEmp_age(employee.getEmp_age());
+			emp.setEmp_city(employee.getEmp_city());
+			emp.setEmp_name(employee.getEmp_name());
+			emp.setEmp_salary(employee.getEmp_salary());
+			employeerepository.save(emp);
 			return "success";
 		}
 		return "Not success";
+	}
+	@DeleteMapping("/employees/{empid}")
+	public String delete(@PathVariable Long empid) {
+		Optional<Employee> emp=employeerepository.findById(empid);
+		if(emp.isPresent()) {
+			employeerepository.deleteById(empid);
+			return "DELETED SUCCESSFULLY";
+		}
+		return "Not Exist";
+	}
+	@DeleteMapping("/employees")
+	public String deleteall() {
+		employeerepository.deleteAll();
+		return "DELETED SUCCESSFULLY";
 	}
 	
 
